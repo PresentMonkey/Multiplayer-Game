2cd281a392d92377e86354dd2a5f861324ae5645
PresentMonkey <gscience213@gmail.com>
2020-12-01 22:21:40 -0500

    Add better comments

--- public/game.js
+++ public/game.js
@@ -1,32 +1,24 @@
-//load images
+//proload images
 var img = new Image();
 img.src = "/public/assets/pfp1.png";
 
 
-
 var socket = io();
-socket.on('message', function(data) {
-  console.log(data);
-});
 
-function sendPong(data){
-    socket.emit('message', data);
-}
-
-socket.emit('new player');
-setInterval(function(){
+socket.emit('new player'); //on new connection send a new player "ping" to server
+setInterval(function(){ //60 times a second send movement requests to server
     socket.emit('movement', movement);
 }, 1000/60);
 
 
 
-var movement = {
+var movement = { //object to hold movement requests
     up: false,
     down: false,
     left: false,
     right: false
 }
-document.getElementById("gameCanvas").addEventListener('keydown', function(event){
+document.getElementById("gameCanvas").addEventListener('keydown', function(event){  //function to handle keypresses
     switch(event.keyCode){
         case 65: //A
             movement.left = true;
@@ -42,7 +34,7 @@ document.getElementById("gameCanvas").addEventListener('keydown', function(event
             break;
     }
 }, true);
-document.getElementById("gameCanvas").addEventListener('keyup', function(event){
+document.getElementById("gameCanvas").addEventListener('keyup', function(event){ //function to handle key unpresses
     switch(event.keyCode){
         case 65: //A
             movement.left = false;
@@ -62,31 +54,27 @@ document.getElementById("gameCanvas").addEventListener('keyup', function(event){
 
 
 var canvas = document.getElementById('canvas');
-canvas.width = 800;
+canvas.width = 800; //set how big it should be
 canvas.height = 600;
 var context = canvas.getContext('2d');
-socket.on('state', function(players) {
-  context.clearRect(0, 0, 800, 600);
+socket.on('state', function(players) { //Run everytime a state object is recived from server
+  context.clearRect(0, 0, 800, 600); //Clear canvas
   context.fillStyle = 'black';
-  for (var id in players) {
+  for (var id in players) { //Move through every player object in players object
     var player = players[id];
-    var imageradius = 16;
+    var imageradius = 16; //MOVE TO CENTRALIZED LOCATION (maybe create custom image object with radious in it?)
     context.font = "10px Arial";
     context.textAlign = "center";
-    context.fillText(player.username, player.x, player.y + imageradius + 10);
-    context.drawImage(img, player.x - imageradius, player.y - imageradius);
-    //context.beginPath();
+    context.fillText(player.username, player.x, player.y + imageradius + 10); //Draws username
+    context.drawImage(img, player.x - imageradius, player.y - imageradius); //Draws image
+    //context.beginPath();  //Legacy code to draw dot instead of image
     //context.arc(player.x, player.y, 2, 0, 2 * Math.PI);
     //context.fill();
   }
 });
 
 
-document.getElementById("usernameInputButton").addEventListener("click", function(){
-    var username = document.getElementById("usernameInputForm").value;
-    socket.emit('username', username);
-});
-document.getElementById("usernameInputForm").addEventListener('submit', function(){
-    var username = document.getElementById("usernameInputForm").value;
+document.getElementById("usernameInputButton").addEventListener("click", function(){ //When username submit button is pressed send username to server: bug to fix: make enter on form work as well
+    var username = document.getElementById("usernameInputForm").value; //read form value
     socket.emit('username', username);
 });
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+//include depedencies
 const express = require('express');
 const http = require('http');
 const path = require('path');
@@ -8,47 +10,40 @@ const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
 
-const port = 3000;
+const port = 3000; //port we will listen on
 
 app.set('port', port);
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));  //set static route for all requests to public (needed for filed inside public be accessible)
 
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //Sends index.html on page load
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-server.listen(port, function(){
+server.listen(port, function(){  
   console.log(`Starting server on port ${port}`)
 })
 
-io.on('connection', function(socket){
-  socket.on('message', function(data){
-    console.log(data);
-  });
-});
-
-
-var players = {};
+var players = {}; //player object (maybe use redis for this later???)
 
 io.on('connection', function(socket){
   socket.on('new player', function(){
-    players[socket.id] = {
+    players[socket.id] = { //When a new player connects, thier socket id will be used as a player ID and placed in the global player object with some predetermined protoype
       x: 400,
       y: 300,
       username: " ", //placeholder for it to not show undefined
     };
   });
-  socket.on('username', function(data){
+  socket.on('username', function(data){ //On username websocket sent add thier username to their player object
       players[socket.id].username = data;
   });
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function(){ //Delete specific player's object on disconnect (maybe save to database (mongodb?) later??, needed right now for their circle to disappear)
     delete players[socket.id];
   });
-  socket.on('movement', function(data) {
-    var player = players[socket.id] || {};
-    var imageradius = 16;
-    if (data.left && player.x > 0 + imageradius) {
+  socket.on('movement', function(data) { //function to handle player movement
+    var player = players[socket.id] || {}; //Don't know what this does??
+    var imageradius = 16; //Move this to a better location later?
+    if (data.left && player.x > 0 + imageradius) { 
       player.x -= 5;
     }
     if (data.up && player.y > 0 + imageradius) {
@@ -66,6 +61,6 @@ io.on('connection', function(socket){
 });
 
 
-setInterval(function(){
+setInterval(function(){  //60 times a second send the whole players object to every connection (maybe impement interpolation later for less "lag")
   io.sockets.emit('state', players);
 }, 1000/60);
