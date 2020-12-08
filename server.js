9a5db5509c15ca42ffbf7421717a77463e53d236
Justin B <justin3926@gmail.com>
2020-12-08 15:00:09 +0000

    Preferred Setup but completely broken

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ server.listen(port, function () {
 
 var worldoneplayer = {}; //player object (maybe use redis for this later???)
 var worldtwoplayer = {};
-var worldchange = 1;
+var worldchange;
 
 io.on('connection', function (socket) {
   var p = 0;
@@ -41,7 +41,7 @@ io.on('connection', function (socket) {
         y: 300,
         username: " ", //placeholder for it to not show undefined
         avatar: "Alien",
-        world: 1
+        world: ''
       };
     });
 
@@ -66,57 +66,47 @@ io.on('connection', function (socket) {
 
 
       var imageradius = 16; //Move this to a better location later?
-      
-      if (worldchange == 1) {
-        var player = worldoneplayer[socket.id] || {}; //Don't know what this does??
-        if (data.left && player.x > 0 + imageradius) {
-          player.x -= 3;
-        }
-        if (data.up && player.y > 0 + imageradius) {
-          player.y -= 3;
-        }
-        if (data.right && player.x < 1920 - imageradius) {
-          player.x += 3;
-        }
-        if (data.down && player.y < 1080 - imageradius) {
-          player.y += 3;
-        }
-        player.world = 1;
-        if (data.interact && player.x > 172 && player.x < 226 & player.y > 234 && player.y < 336) { //press e
-          player.world = 2;
-          socket.join('worldtwo');
-          socket.leave('worldone');
-          worldchange = 2;
-        }
+
+      var player = worldplayer[socket.id] || {}; //Don't know what this does??
+      if (data.left && player.x > 0 + imageradius) {
+        player.x -= 3;
       }
-      if (worldchange == 2) {
-        player = worldtwoplayer[socket.id] || {}; //Don't know what this does??
-        if (data.left && player.x > 0 + imageradius) {
-          player.x -= 3;
-        }
-        if (data.up && player.y > 0 + imageradius) {
-          player.y -= 3;
-        }
-        if (data.right && player.x < 1920 - imageradius) {
-          player.x += 3;
-        }
-        if (data.down && player.y < 1080 - imageradius) {
-          player.y += 3;
-        }
+      if (data.up && player.y > 0 + imageradius) {
+        player.y -= 3;
+      }
+      if (data.right && player.x < 1920 - imageradius) {
+        player.x += 3;
+      }
+      if (data.down && player.y < 1080 - imageradius) {
+        player.y += 3;
+      }
+      player.world = 1;
+      if (data.interact && player.x > 172 && player.x < 226 & player.y > 234 && player.y < 336) { //press e
         player.world = 2;
-        if (data.tester && player.x > 490 && player.x < 574 & player.y > 300 && player.y < 400) { //press q
-          player.world = 1;
-          socket.join('worldone');
-          socket.leave('worldtwo');
-          worldchange = 1;
-        }
+        worldchange = 2;
+        return;
       }
+
+      if (data.tester && player.x > 490 && player.x < 574 & player.y > 300 && player.y < 400) { //press q
+        player.world = 1;
+        worldchange = 1;
+        return;
+      }
+
 
     });
   }
-
-  createplayers(worldoneplayer);
-  createplayers(worldtwoplayer);
+  if (worldchange === 1) {
+    socket.join('worldone');
+    socket.leave('worldtwo');
+    
+    createplayers(worldoneplayer);
+  }
+  if (worldchange === 2) {
+    socket.join('worldtwo');
+    socket.leave('worldone');
+    createplayers(worldtwoplayer);
+  }
 
 });
 setInterval(function () {  //send players every 60sec
